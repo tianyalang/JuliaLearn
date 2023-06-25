d8abc7156b9e21b1b7871e1b6281a71ed3c3f1a7
HeZudao <44080909+tianyalang@users.noreply.github.com>
2023-06-25 23:31:46 +0800

    Update 01.jl

--- srm/01.jl
+++ srm/01.jl
@@ -1,3 +1,4 @@
+# 输入参数
 SN = 320  # 额定容量/kVA
 UN = 400  # 额定电压/V
 cosphi = 0.8
@@ -24,8 +25,16 @@ Nt = 6    # 并绕根数
 d = 1.4   # 裸导线直径/mm
 dc = 1.479 # 导线直径/mm
 
+S = 3.7  # 槽口宽
+B = 6.7
+B1 = 8.7
+H = 1.07 # 槽口深
+T = 42
+
+# 计算参数
 p = 60f / nN # 极对数
 IN = SN * 1e3 / sqrt(3) / UN # 额定电流/A
+D2 = Di1 - 2δ
 bkp = bk^2 / (bk + 5δ / 2) # 适用于定转子通风道对齐情况
 Leff = Lt - nk * bkp # 有效铁芯长
 LFet = (Lt - nk * bk) * KFe # 净铁芯长
@@ -35,7 +44,7 @@ dC = dA - dB  # 大齿占槽数
 q = Z1 / (2 * p * m)  # 每极每相槽数
 β = y1 / (m * q)    # 节距系数
 Kp = sin(β * pi / 2)  # 短距系数
-Kd = sin(pi / 2 / m) / q / sin(pi / 2 / m / q) # 绕组分布系数
+Kd = sin(pi / 2m) / q / sin(pi / 2m / q) # 绕组分布系数
 Kdp = Kp * Kd  # 绕组系数
 N = Ns * Z1 / m / a  # 每相串联导体数
 qa = pi * d^2 / 4  # 导线截面积
@@ -43,3 +52,16 @@ ja = IN / (a * Nt * qa) # 定子绕组电密 A/mm^2
 A = m * N * IN / pi / Di1 # 线负荷
 Aj = A * ja
 
+As = (B1+B)/2*(T - B1/2) + pi*(B1/2)^2/2 # 槽面积，不含槽口
+Ai = 52 #todo
+Ae = As - Ai
+Sf = Ns*Nt*pi*(dc/2)^2/Ae # 槽满率
+
+ts = pi*Di1/Z1  # 定子齿距
+bt11 = pi*(Di1 + 2H)/Z1 - B
+bt12 = pi*(Di1 + 2*(H + T - B1/2))/Z1 - B1
+bmax = max(bt11, bt12)
+bmin = min(bt11, bt12)
+bt1 = (bmax + 2bmin)/3
+
+ht1p = T - B1/2 + B1/2/3 #定子齿磁路计算长度
