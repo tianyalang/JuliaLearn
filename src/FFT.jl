054122b1fe3527f617334ea7249b2541153dd247
tianyalang <44080909+tianyalang@users.noreply.github.com>
2024-03-25 20:09:17 +0800

    read .csv file

--- src/FFT.jl
+++ src/FFT.jl
@@ -9,7 +9,7 @@ S = 0.7 * sin.(2 * pi * 50 * t) + sin.(2 * pi * 120 * t)
 #用均值为零、方差为 4 的白噪声扰乱该信号。
 X = S + 2 * randn(size(t))
 subplot(2, 1, 1)
-plot(1000 * t[1:50], X[1:50])
+plot(1000 * t[1:50], X[1:50]) 
 title("均值为0的随机噪声信号")
 xlabel("t(milliseconds)")
 ylabel("X(t)")
---
+++ src/csvRead.jl
@@ -0,0 +1,3 @@
+using CSV;
+using DataFrames;
+xx = CSV.read("./data/Winding Plot 3.csv", DataFrame)
