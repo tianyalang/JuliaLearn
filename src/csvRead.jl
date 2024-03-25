7a9286b81b50b4702b1dce04ab684414ea1f6c32
HeZudao <44080909+tianyalang@users.noreply.github.com>
2024-03-25 23:20:17 +0800

    update

--- src/csvRead.jl
+++ src/csvRead.jl
@@ -1,3 +1,12 @@
 using CSV;
 using DataFrames;
-xx = CSV.read("./data/Winding Plot 3.csv", DataFrame)
+xx = CSV.read("./data/Winding Plot 3.csv", DataFrame)  # A,C 相电压时域波形
+yy = CSV.read("./data/Winding Plot 4.csv", DataFrame)  # AC线电压频域波形 
+
+t = xx[:, 1]
+X = xx[:, 2] - xx[:, 3]
+
+subplot(2,1,1)
+plot(t, X)
+subplot(2,1,2)
+plot(yy[3:end,2], yy[3:end,3])
