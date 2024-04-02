b14c7f06c66713d7c6289f76a53ad22502945fb3
HeZudao <44080909+tianyalang@users.noreply.github.com>
2024-04-03 07:09:16 +0800

    update

--- src/randomSampleSignal.jl
+++ src/randomSampleSignal.jl
@@ -5,12 +5,12 @@ L = 1500
 t = (0:L-1) * T
 
 #构造一个信号，其中包含幅值为 0.7 的 50 Hz 正弦量和幅值为 1 的 120 Hz 正弦量。
-S = 0.7 * sin.(2 * pi * 50 * t) + sin.(2 * pi * 120 * t)
+S = 0.7 * sin.(2 * pi * 50 * t) + sin.(2 * pi * 120 * t) + 0.5*sin.(2*pi*300*t.+pi/6)
 #用均值为零、方差为 4 的白噪声扰乱该信号。
 X = S + 2 * randn(size(t))
 
 
-plot(1000 * t[1:100], X[1:100])
+plot(t[1:100], X[1:100], "-o")
 title("均值为0的随机噪声信号")
-xlabel("t(milliseconds)")
+xlabel("t(seconds)")
 ylabel("X(t)")
